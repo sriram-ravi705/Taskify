0a0ddab0545415d3324c25f647d1ea5b99fcc248
Sri Ram <sriramravi477@gmail.com>
2025-03-01 19:29:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mysql = require('mysql2');
 const path = require('path');
 const app = express();
-const port = 3000;
+const port = 3001;
 
 // Load environment variables
 require('dotenv').config();
